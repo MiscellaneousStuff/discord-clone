1238b4ba5c98cce9474a5646c80447d5335cf910
Joe Makepeace <joe.makepeace01@gmail.com>
2022-05-10 00:57:07 +0100

    added basic messsage and messages components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const testMessages = ["hi", "hello!", "so, how are ya ?"];
+
+const Message = props => {
+  const { message } = props;
+  return (
+    <div>
+      {message}
+    </div>
+  )
+}
+
+const Messages = props => {
+  const { messages } = props;
+  return (
+    <div>
+      {messages.map((message, i) => <Message message={message}/>)}
+    </div>
+  )
+};
+
+const App = () => {
   return (
     <div className="app">
-      asd
+      <Messages messages={testMessages} />
     </div>
   );
 }
