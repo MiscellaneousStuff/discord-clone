470fb1289d4957812b064a39db9fa34700a4ecba
Joe Makepeace <joe.makepeace01@gmail.com>
2022-05-10 01:09:29 +0100

    configurable host ip

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const host = "localhost";
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, host, () => {
+  console.log(`Example app listening on port ${host}:${port}`)
 })
